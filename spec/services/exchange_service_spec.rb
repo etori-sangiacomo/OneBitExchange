28de72f1227b68ba4fa53edc5605be3f3dfa19fe
Etori Badanai <etoribadanai@gmail.com>
2019-02-17 09:58:03 -0300

    creating exchange-request-test

--- spec/services/exchange_service_spec.rb
+++ spec/services/exchange_service_spec.rb
@@ -7,7 +7,6 @@ describe 'Currency' do
     amount = rand(0..9999)
     value = 100
     res = ExchangeService.new("USD", "BRL", value).perform
-    byebug
     expect(res.is_a? Numeric).to eql(true)
     expect(res != 0 || amount == 0).to eql(true)
   end
